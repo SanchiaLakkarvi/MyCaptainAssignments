96dc3c368e5a785ee60befc535b8786026c99b83
SanchiaLakkarvi <110339165+SanchiaLakkarvi@users.noreply.github.com>
2022-07-31 19:08:40 +0530

    Create Area.py

---
+++ Area.py
@@ -0,0 +1,5 @@
+#python program to find the circumference of the circle
+Pi=3.14
+radius=float(input("enter the radius to find the area of the circle\n"))
+area = Pi * radius * radius
+print("the area of the circle is:",area)
